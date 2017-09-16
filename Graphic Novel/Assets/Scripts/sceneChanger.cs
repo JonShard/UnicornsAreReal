9925607118ca77edc4800dc8a3ccdf57e62ef05e
Cynical_Penguin <Erlendvh@stud.ntnu.no>
2017-09-16 10:54:55 +0200

    changed name of the script, added scenes, added scenes to build settings

--- Graphic Novel/Assets/Scripts/sceneChanger.cs
+++ Graphic Novel/Assets/Scripts/sceneChanger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class NewBehaviourScript : MonoBehaviour
+public class sceneChanger : MonoBehaviour
 {
 
 	// Use this for initialization
