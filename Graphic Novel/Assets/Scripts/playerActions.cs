1a39f59ac4d2c03cf22b602006a9c7206746b727
Arcarnan <nataniel1993@hotmail.com>
2017-09-16 19:29:24 +0200

    cleaned up in the playerAction script

--- Graphic Novel/Assets/Scripts/playerActions.cs
+++ Graphic Novel/Assets/Scripts/playerActions.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 
 public class playerActions : MonoBehaviour
 {
-
     public Vector2 desiredPosition = new Vector2(0, 0);             //The global position the player will move to. 
-    public float movementSpeed = 3.0f;                             //How fast the player moves to the disired location.
+    public float movementSpeed = 3.0f;                              //How fast the player moves to the disired location.
     private int movingInDirection = 0;                              //0 = still,  1 = right,  2 = left,  3 = up,  4 = down
 
     private Animator animator;
-
     public Rigidbody2D playerRigidbody;
-
-    int rightHash = Animator.StringToHash("movementRight");
-    int leftHash = Animator.StringToHash("movementLeft");
-    int upHash = Animator.StringToHash("movementUpward");
-    int downHash = Animator.StringToHash("movementForward");
-    int idle = Animator.StringToHash("defaultState");
 
     // Use this for initialization
     void Start()
@@ -32,6 +24,7 @@ public class playerActions : MonoBehaviour
     {
         //Constantly move player to the desired position with movement speed.
         Vector2 vectorToDesiredPoint = desiredPosition - new Vector2(transform.position.x, transform.position.y);
+
         if (vectorToDesiredPoint.magnitude > 0.1f)
         {
             playerRigidbody.velocity = vectorToDesiredPoint;
@@ -40,24 +33,19 @@ public class playerActions : MonoBehaviour
             int angle = (int)Helpfunctions.getAngle(vectorToDesiredPoint, new Vector2(0, 0)) + 90;
             if (angle > 180 - 45 && angle < 180 + 45)
             {
-                movingInDirection = 1;          //right
-                //animator.SetTrigger(movingInDirection);
-                //animator.SetInteger("direction", movingInDirection);
+                movingInDirection = 1;     //right
             }
             else if (angle > 0 - 45 && angle < 0 + 45)
             {
                 movingInDirection = 2;     //left
-               // animator.SetTrigger(leftHash);
             }
             else if (angle > 270 - 45 && angle < 270 + 45)
             {
-                movingInDirection = 3;      //up
-               // animator.SetTrigger(upHash);
+                movingInDirection = 3;     //up
             }
             else if (angle > 90 - 45 && angle < 90 + 45)
             {
-                movingInDirection = 4;      //down
-                //animator.SetTrigger(downHash);
+                movingInDirection = 4;     //down
             }
             animator.SetInteger("direction", movingInDirection);
         }
@@ -67,16 +55,11 @@ public class playerActions : MonoBehaviour
             animator.SetInteger("direction", movingInDirection);
             playerRigidbody.velocity = Vector2.zero;
         }
-        //animator.SetInteger("direction", movingInDirection);
         Debug.Log("Direction = " + movingInDirection);
     }
-
-
+    
     void moveToPosition(Vector2 v)
     {
         desiredPosition = v;                                        //Sets the desired position to be a point on global cordinates. 
     }
-
-
-
 }
