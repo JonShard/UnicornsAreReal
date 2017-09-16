ca35c7aa7ff6ddde8dec432e766faade941a5363
JonShard <jonems@stud.ntnu.no>
2017-09-16 16:48:20 +0200

    Added a game over screen.

---
+++ Graphic Novel/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    SpriteRenderer backgroundSpriteRenderer;
+    GUIText deathReasonText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        enabled = false;
+        deathReasonText = GetComponentInChildren<GUIText>();
+	}
+
+
+    public void activateGameOverScreen(string deathReason)
+    {
+        deathReasonText.text = deathReason;
+        enabled = true;
+    }
+
+    public void deactivateGameOverScreen()
+    {
+        enabled = false;
+    }
+}
