a5eb213f4eeb1e5519d2863d465bc6763eabbdb6
Arcarnan <nataniel1993@hotmail.com>
2017-09-16 20:55:53 +0200

    added movement to the dragon script, cleaned in the dragon script, minor changes to playerAction script

--- Graphic Novel/Assets/dragonActions.cs
+++ Graphic Novel/Assets/dragonActions.cs
@@ -4,47 +4,52 @@ using UnityEngine;
 
 public class dragonActions : MonoBehaviour
 {
+    public int dragonAction = 0;    //  0 = idle, 1 = attack, 2 = death, 3 = flight/ movement
+    public float movementSpeed = 5.0f;                              //How fast the player moves to the disired location.
 
     private Animator animator;
     public Rigidbody2D dragonRigidbody;
-    public int dragonAction = 0;    //  0 = idle, 1 = attack, 2 = death, 3 = flight
-
-    // Use this for initialization
+    public Vector2 desiredPosition = new Vector2(0, 0);             //The global position the player will move to. 
+    
     void Start()
     {
         dragonRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        //move dragon to the desired position with movement speed.
+        Vector2 vectorToDesiredPoint = desiredPosition - new Vector2(transform.position.x, transform.position.y);
         int dragonAnimation = 0;
+
+        dragonRigidbody.velocity = vectorToDesiredPoint;
+        dragonRigidbody.velocity = dragonRigidbody.velocity.normalized * movementSpeed;
 
         switch (dragonAction)
         {
-            case 0: dragonAnimation = 0;
+            case 0: dragonAnimation = 0;    //  idle
                 break;
 
             case 1:
-                dragonAnimation = 1;
+                dragonAnimation = 1;    //  attack
                 break;
 
             case 2:
-                dragonAnimation = 2;
+                dragonAnimation = 2;    //  death
                 break;
 
             case 3:
-                dragonAnimation = 3;
-                break;
-
-            case 4:
-                dragonAnimation = 4;
+                dragonAnimation = 3;    //  flight/ movement
                 break;
 
             default: dragonAnimation = 0;
                 break;
         }
         animator.SetInteger("dragonAction", dragonAnimation);
+    }
+    public void moveDragonToPosition(Vector2 v)
+    {
+        desiredPosition = v;                                        //Sets the desired position to be a point on global cordinates. 
     }
 }
